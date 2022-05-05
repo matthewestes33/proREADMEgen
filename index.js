55db4226fab4d60e57f3cae86cd58635826abdfb
Matt Estes <me6384@gmail.com>
2022-05-04 19:33:42 -0700

    updates to more functional README

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description explaining the what, why, and how of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project? Provide a description of how to get the development environment running.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please list the license that you are using. This lets other developers know what they can and cannot do with your project.',
+        choices: ['The MIT License', 'ISC License (ISC)', 'IBM Public License Version 1.0', 'none']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'List your collaborators, if any, with links to their GitHub profiles',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Include the instructions for your tests here.',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Allow users to contact you if they have additional questions.',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function generateMarkdown(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        console.log(data)
+        const output = generateMarkdown(data);
+        console.log(output)
+        fs.writeFile('README.md', output, (err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log('README created!');
+        });
+    });
+}
 
 // Function call to initialize app
 init();
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,96 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseSection(license) {
+  if(license === 'none') {
+    return '';
+  }
+  else{ 
+    return `## License ${license}`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'none') {
+    return '';
+  }
+  if(license === 'IBM Public License Version 1.0') {
+    return '[IBM Public License Version 1.0](https://opensource.org/licenses/IPL-1.0)'
+  }
+  if(license === 'ISC License (ISC)') {
+    return '[ISC License (ISC)](https://opensource.org/licenses/ISC)'
+  }
+  if(license === 'The MIT License') {
+    return '[The MIT License](https://opensource.org/licenses/MIT)'
+  }
 
-// TODO: Create a function that returns the license section of README
+}
+
+// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'none') {
+    return '';
+  }
+  if(license === 'IBM Public License Version 1.0') {
+    return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+  }
+  if(license === 'ISC License (ISC)') {
+    return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+  }
+  if(license === 'The MIT License') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+
+// TODO: Create a function to generate markdown for README (using Professional README Guide https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+ 
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contributing}
+
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseBadge(data.license)}
+  ${renderLicenseLink(data.license)}
+
+  ## Badges
+  
+  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+  
+  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  
+  ## Features
+  
+  If your project has a lot of features, list them here.
+  
+  ## How to Contribute
+  
+  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+  
+  ## Tests
+  
+  Go the extra mile and write tests for your application. Then provide examples on how to run them here.
 `;
 }
 
